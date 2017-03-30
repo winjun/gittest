94d217f5565239c6ab57b6eaefdf1bbbcb0ce116
winjun <winjun1@naver.com>
2017-03-30 20:41:58 +0900

    chain

---
+++ gill_test/src/com/gill/pattern/chain/ConsoleLogger.java
@@ -0,0 +1,13 @@
+package com.gill.pattern.chain;
+
+public class ConsoleLogger extends AbstractLogger {
+
+   public ConsoleLogger(int level){
+      this.level = level;
+   }
+
+   @Override
+   protected void write(String message) {		
+      System.out.println("Standard Console::Logger: " + message);
+   }
+}
